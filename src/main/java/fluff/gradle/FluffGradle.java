e1c4be7fc65849f397458f14e7145ad4ab5c2864
Musca Mihail <muscamihaill@gmail.com>
2024-04-23 21:41:53 +0300

    First Commit

---
+++ src/main/java/fluff/gradle/FluffGradle.java
@@ -0,0 +1,14 @@
+package fluff.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import fluff.gradle.tasks.TaskInfo;
+
+public class FluffGradle implements Plugin<Project> {
+	
+	@Override
+	public void apply(Project project) {
+		project.getTasks().register("info", TaskInfo.class);
+	}
+}
---
+++ src/main/java/fluff/gradle/tasks/TaskInfo.java
@@ -0,0 +1,12 @@
+package fluff.gradle.tasks;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class TaskInfo extends DefaultTask {
+	
+	@TaskAction
+	public void onAction() {
+		System.out.println("Fluff Gradle plugin installed!");
+	}
+}
